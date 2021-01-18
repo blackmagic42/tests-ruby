1991e5c11d934db574c2c5de7982abaab706264f
blackmagic-42 <david.giraudeau@yahoo.fr>
2021-01-18 19:17:30 +0100

    dégagez mes freros

---
+++ spec/pig_latin_spec.rb
@@ -0,0 +1,44 @@
+require_relative '../lib/pig_latin'
+
+describe "#translate" do
+
+  it "translates a word beginning with a vowel" do
+    s = translate("apple")
+    expect(s).to eq("appleay")
+  end
+
+  it "translates a word beginning with a consonant" do
+    s = translate("banana")
+    expect(s).to eq("ananabay")
+  end
+
+  it "translates a word beginning with two consonants" do
+    s = translate("cherry")
+    expect(s).to eq("errychay")
+  end
+
+  it "translates two words" do
+    s = translate("eat pie")
+    expect(s).to eq("eatay iepay")
+  end
+
+  it "translates a word beginning with three consonants" do
+    expect(translate("three")).to eq("eethray")
+  end
+
+  it "counts 'sch' as a single phoneme" do
+    s = translate("school")
+    expect(s).to eq("oolschay")
+  end
+
+  it "counts 'qu' as a single phoneme" do
+    s = translate("quiet")
+    expect(s).to eq("ietquay")
+  end
+
+  it "translates many words" do
+    s = translate("the quick brown fox")
+    expect(s).to eq("ethay ickquay ownbray oxfay")
+  end
+
+end
